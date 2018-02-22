7f9c892fc4a80b05667d981a119dd811a3f58e34
Anders Begtorp Jensen <au558782@uni.au.dk>
2018-02-22 15:26:37 +0100

    Colorbet testcases added

--- RouletteGame.Test.Unit/ColorFieldTestUnit.cs
+++ RouletteGame.Test.Unit/ColorFieldTestUnit.cs
@@ -21,18 +21,21 @@ namespace RouletteGame.Test.Unit
         }
 
         [TestCase("Anders",(uint)500,(uint)1,(uint)1000)]
+        [TestCase("Anders", (uint)5000, (uint)1, (uint)10000)]
+        [TestCase("Anders", (uint)50000, (uint)1, (uint)100000)]
+
         public void WonAmount_ColorChosen_CorrectAmount(string name, uint amount, uint color, uint expectedReturn)
         {
             _uut = new ColorBet(name, amount,color);
-
             Assert.That(_uut.WonAmount(new Field(2,color)),Is.EqualTo(expectedReturn));
         }
 
+        [TestCase("Anders", (uint)500, (uint)0, "red")]
         [TestCase("Anders", (uint)500, (uint)1, "black")]
+        [TestCase("Anders", (uint)500, (uint)2, "green")]
         public void ToString_ReturnColor_ColorIsCorrect(string name, uint amount, uint color, string expectedReturn)
         {
             _uut = new ColorBet(name, amount, color);
-
             Assert.That(_uut.ToString(),Is.EqualTo(amount+ "$ color bet on " + expectedReturn));
         }
     }
