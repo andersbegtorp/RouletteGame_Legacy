5c7e700f4a397953daf29a31630ed4b56b8a4146
Anders Begtorp Jensen <au558782@uni.au.dk>
2018-02-22 15:09:29 +0100

    Colorbet Test Added

---
+++ RouletteGame.Test.Unit/ColorFieldTestUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RouletteGame.Legacy;
+using NSubstitute;
+
+namespace RouletteGame.Test.Unit
+{
+    [TestFixture]
+
+    public class ColorFieldTestUnit
+    {
+        private ColorBet _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [TestCase("Anders",(uint)500,(uint)1,(uint)1000)]
+        public void WonAmount_ColorChosen_CorrectAmount(string name, uint amount, uint color, uint expectedReturn)
+        {
+            _uut = new ColorBet(name, amount,color);
+
+            Assert.That(_uut.WonAmount(new Field(2,color)),Is.EqualTo(expectedReturn));
+        }
+    }
+}
+
