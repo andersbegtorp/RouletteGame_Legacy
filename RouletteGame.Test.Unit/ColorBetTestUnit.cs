caa1ae5846db9c6138488433adddd4d4d4b1f471
Anders Begtorp Jensen <au558782@uni.au.dk>
2018-02-22 14:11:04 +0100

    TestColor

---
+++ RouletteGame.Test.Unit/ColorBetTestUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RouletteGame.Legacy;
+using NSubstitute;
+
+namespace RouletteGame.Test.Unit
+{
+    [TestFixture]
+    public class ColorBetTestUnit
+    {
+        private ColorBet _uut;
+        private IBet _bet;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bet = 
+        }
+
+        [TestCase]
+        []
+        public void Color_WonAmount_IsCorrect()
+        {
+            
+        }
+
+    }
+}
